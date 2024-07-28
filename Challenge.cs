0e1b689fb36b5268ddd058ccbe1b7db2e60887b8
Jagjeet Singh <42211757+Jagjeet229@users.noreply.github.com>
2024-07-28 10:02:00 -0400

    Update Challenge.cs

--- Challenge.cs
+++ Challenge.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,9 +12,9 @@ public class OldPhone
         Console.WriteLine (OldPhonePad("227*#"));
         Console.WriteLine (OldPhonePad("4433555 555666##"));
         Console.WriteLine (OldPhonePad("8 88777444666*664#"));
-        
+
     }
-    
+
   public static string OldPhonePad(string input ){
     Dictionary<char, string[]> wordDictionary = new Dictionary<char, string[]>();
     wordDictionary.Add('2', new string[] {"A","B","C"});
@@ -26,19 +25,19 @@ public class OldPhone
     wordDictionary.Add('7', new string[] {"P","Q","R","S"});
     wordDictionary.Add('8', new string[] {"T","U","V"});
     wordDictionary.Add('9', new string[] {"W","X","Y","Z"});
-    
+
 
     StringBuilder answer = new StringBuilder();
-    
+
     int count =0;
     char lastChar = '\0';
     for(int i=0;i<input.Length;i++){
-       
-      if(lastChar=='\0'){
-          lastChar = input[i];
+
+      if(lastChar=='\0' && input[i]!= '#'){
+          lastChar = input[i]; 
           count++;
       } 
-       
+
       else if(input[i] == lastChar){
           count = count+1;
       }
@@ -47,19 +46,23 @@ public class OldPhone
           count=0;
       }
       else if(input[i] == ' ' || input[i] == '0'){
-          answer.Append(wordDictionary[lastChar][count-1]);
-          lastChar = input[i+1];
+          if(lastChar!= '\0'){
+            answer.Append(wordDictionary[lastChar][count-1]);
+          }
+          lastChar = '\0';
           count=0;
       }
       else if(input[i] == '#'){
-          answer.Append(wordDictionary[input[i-1]][count-1]);
+          if(lastChar!= '\0'){
+            answer.Append(wordDictionary[lastChar][count-1]);
+          }
           return answer.ToString();
       }
       else if(input[i]!= lastChar){
-          answer.Append(wordDictionary[input[i-1]][count-1]);
+          answer.Append(wordDictionary[lastChar][count-1]);
           lastChar = input[i];
           count=1;
-         
+
       }
     }
     return answer.ToString();
